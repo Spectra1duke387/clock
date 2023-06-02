8255515f1495946269d48cdcc3df011e88eca228
Matt Amend <matthew.amend@aol.com>
2023-06-01 20:36:25 -0400

    Update Makefile & refactor usage() in clock.c

--- Makefile
+++ Makefile
@@ -3,4 +3,6 @@ CFLAGS=-Wall -D_XOPEN_SOURCE_EXTENDED
 CLIBS=-lncursesw
 
 all:
-	@$(CC) $(CFLAGS) -o /bin/clock src/clock.c $(CFLAGS) $(CLIBS)
+	@$(CC) $(CFLAGS) -o /bin/clock src/clock.c  $(CLIBS)
+local:
+	@$(CC) $(CFLAGS) -o clock src/clock.c $(CLIBS)
--- src/clock.c
+++ src/clock.c
@@ -14,33 +14,7 @@
 #define COLOR_SET 1
 
 // TODO: getopt_long stuff :-)
-void usage(void)
-{
-	printf("Usage: clock [-h] [-c=color] [--version]");
-	printf(" [--help] [--hide-exit-text]\n\n");
-
-	printf("clock is an ASCII art clock designed to be a part");
-	printf(" of a terminal multiplex setup.\n");
-	printf("The clock displayed is always printed");
-	printf(" to the center of a window. Keep in \n");
-	printf("mind that the clock can be up to 1 second");
-	printf(" off and the time displayed is that\n");
-	printf("of your local timezone. Different timezones");
-	printf(" are to be added at a later date.\n\n");
-	printf("commands:\n");
-	printf("\t-h\t--help\t\tprint this information\n");
-	printf("\t-c [color]\t\tset clock to different color\n");
-	printf("\t\tred\n");
-	printf("\t\tgreen\n");
-	printf("\t\tyellow\n");
-	printf("\t\tblue\n");
-	printf("\t\tmagenta\n");
-	printf("\t\tcyan\n");
-	printf("\t\twhite (default)\n");
-	printf("\t--version\t\tprint current version (0.9.1)\n");
-	printf("\t--hide-exit-text\thide text info");
-	printf(" at the bottom of the screen\n");
-}
+void usage(void);
 
 int main(int argc, char **argv)
 {
@@ -138,4 +112,33 @@ int main(int argc, char **argv)
 		nanosleep((const struct timespec[]){{0, 500000000L}}, NULL);
 	}
 	endwin();
+}
+
+
+void usage(void)
+{
+	printf("Usage: clock [-h] [-c=color] [--version]");
+	printf(" [--help] [--hide-exit-text]\n\n");
+
+	printf("clock is an ASCII art clock designed to be a part");
+	printf(" of a terminal multiplex setup.\n");
+	printf("The clock displayed is always printed");
+	printf(" to the center of a window. Keep in \n");
+	printf("mind that the clock can be up to 1 second");
+	printf(" off and the time displayed is that\n");
+	printf("of your local timezone. Different timezones");
+	printf(" are to be added at a later date.\n\n");
+	printf("commands:\n");
+	printf("\t-h\t--help\t\tprint this information\n");
+	printf("\t-c [color]\t\tset clock to different color\n");
+	printf("\t\tred\n");
+	printf("\t\tgreen\n");
+	printf("\t\tyellow\n");
+	printf("\t\tblue\n");
+	printf("\t\tmagenta\n");
+	printf("\t\tcyan\n");
+	printf("\t\twhite (default)\n");
+	printf("\t--version\t\tprint current version (0.9.1)\n");
+	printf("\t--hide-exit-text\thide text info");
+	printf(" at the bottom of the screen\n");
 }
